55b8236fee9b04a4cec4552c2c5a3fff73457238
tesla33io <artemstavropolov@gmail.com>
2023-12-21 21:26:40 +0100

    feat: add dirs and files count.

--- tree-rust/src/main.rs
+++ tree-rust/src/main.rs
@@ -1,7 +1,15 @@
-use std::fs;
 use colored::*;
+use std::fs;
 
-fn print_dir_content(path: &str, ntabs: Option<usize>, curr_layer: Option<i32>, max_layer: Option<i32>) {
+fn print_dir_content(
+    path: &str,
+    ntabs: Option<usize>,
+    curr_layer: Option<i32>,
+    max_layer: Option<i32>,
+) -> Option<(i32, i32)> {
+    let mut ndirs = 0;
+    let mut nfiles = 0;
+
     if let Ok(entries) = fs::read_dir(path) {
         for entry in entries {
             if let Ok(entry) = entry {
@@ -9,29 +17,41 @@ fn print_dir_content(path: &str, ntabs: Option<usize>, curr_layer: Option<i32>, 
                     if let Some(entry_name) = entry.file_name().to_str() {
                         let spaces = " ".repeat(ntabs.unwrap_or(0));
                         if curr_layer == max_layer {
-                            return ;
+                            return Some((ndirs, nfiles));
                         }
                         if metadata.is_dir() {
+                            ndirs += 1;
                             println!("{}{}", spaces, entry_name.blue());
-                            print_dir_content(entry.
-                                                    path().
-                                                    to_str().
-                                                    unwrap_or("-"),
-                                                    Some(ntabs.unwrap_or(0) + 3),
-                                                    Some(curr_layer.unwrap_or(0) + 1),
-                                                    max_layer);
+                            if let Some((sub_ndirs, sub_nfiles)) = print_dir_content(
+                                entry.path().to_str().unwrap_or(""),
+                                Some(ntabs.unwrap_or(0) + 3),
+                                Some(curr_layer.unwrap_or(0) + 1),
+                                max_layer,
+                            ) {
+                                ndirs += sub_ndirs;
+                                nfiles += sub_nfiles;
+                            }
                         } else {
+                            nfiles += 1;
                             println!("{}{}", spaces, entry_name);
                         }
                     }
                 }
             }
         }
+        return Some((ndirs, nfiles));
     } else {
-        println!("Failed to read the directory contents");
+        None
     }
 }
 
 fn main() {
-    print_dir_content(".", Some(0), Some(0), Some(2));
+    if let Some((total_dirs, total_files)) = print_dir_content(
+        ".",
+        Some(0),
+        Some(0),
+        Some(2)
+    ) {
+        println!("\nTotal dirs: {}\nTotal files: {}", total_dirs, total_files);
+    }
 }
